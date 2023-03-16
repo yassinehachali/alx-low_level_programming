ee874710ab076c442486385dd4b84080f592db18
yassinehachali <33522063+yassinehachali@users.noreply.github.com>
2023-03-17 00:33:32 +0100

    Update 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - Entry point
- * Return:1
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
