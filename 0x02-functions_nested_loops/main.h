719122b16914894b0a6b45734755fdc3aa0d0951
yassinehachali <yassinehachali@gmail.com>
2023-03-21 16:05:31 -0700

    test

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,24 +1,21 @@
-main.h CODE
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
+#include <stdio.h>
+#include <stdlib.h>
 
+char *_strdup(char *str);
 
-#ifndef main_h
-	#define main_h
+char **strtow(char *str);
 
+char *argstostr(int ac, char **av);
 
-	int _putchar(char);
-	void print_alphabet(void);
-	void print_alphabet_x10(void);
-	int _islower(int c);
-	int _isalpha(int c);
-	int print_sign(int n);
-	int _abs(int);
-	int print_last_digit(int);
-	void jack_bauer(void);
-	void times_table(void);
-	int add(int, int);
-	void print_to_98(int n);
-	void print_times_table(int n);
+char *str_concat(char *s1, char *s2);
 
+void free_grid(int **grid, int height);
 
-	#endif
+int **alloc_grid(int width, int height);
+
+char *create_array(unsigned int size, char c);
+
+#endif
