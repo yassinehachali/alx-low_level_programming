b7ed213e26c653c739665030695fa1e55334a1e3
yassinehachali <yassinehachali@gmail.com>
2023-03-21 15:46:09 -0700

    first script

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+int main(void)
+{
+  char c = '_';
+  write(1, &c, 1);
+  c = 'p';
+  write(1, &c, 1);
+  c = 'u';
+  write(1, &c, 1);
+  c = 't';
+  write(1, &c, 1);
+  c = 'c';
+  write(1, &c, 1);
+  c = 'h';
+  write(1, &c, 1);
+  c = 'a';
+  write(1, &c, 1);
+  c = 'r';
+  write(1, &c, 1);
+  c = '\n';
+  write(1, &c, 1);
+
+  return (0);
+}
