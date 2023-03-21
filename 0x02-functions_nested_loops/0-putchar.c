77b5805dbe862dc49078bfbbd570ed856702cb42
yassinehachali <yassinehachali@gmail.com>
2023-03-21 15:50:17 -0700

    script1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,22 @@
-#include <unistd.h>
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
 
-int main(void)
-{
-  char c = '_';
-  write(1, &c, 1);
-  c = 'p';
-  write(1, &c, 1);
-  c = 'u';
-  write(1, &c, 1);
-  c = 't';
-  write(1, &c, 1);
-  c = 'c';
-  write(1, &c, 1);
-  c = 'h';
-  write(1, &c, 1);
-  c = 'a';
-  write(1, &c, 1);
-  c = 'r';
-  write(1, &c, 1);
-  c = '\n';
-  write(1, &c, 1);
 
-  return (0);
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
 }
